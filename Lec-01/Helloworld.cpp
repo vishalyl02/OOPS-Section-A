a98c23304045472042d0f051cf20cc03ede5c10e
vishalyl02 <vishalyadavvns12345@gmail.com>
2025-01-29 11:38:24 +0530

    First Commit

---
+++ Lec-01/Helloworld.cpp
@@ -0,0 +1,13 @@
+#include<iostream>    // Header File   input output stream
+
+using namespace std;              // Using Standard Library 
+
+int main()                   // Start of the Program
+{
+
+cout<<" Hello world "<<endl;             // Task-> Printing Hello world on Console
+
+return 0;                      // End of the Program   
+}
+
+
---
+++ Lec-01/SumofTwoNumbers.cpp
@@ -0,0 +1,19 @@
+#include<iostream>
+using namespace std;
+
+
+int main()
+{
+
+int a,b;     // Declaring two integer variables
+cin>>a>>b;   // Taking using input    
+
+
+int sum=a+b;      //     Declaring a sum variable that stores the sum of the values of a and b
+
+cout<<sum<<endl;    // Task -> Printing the sum on the console
+
+
+return 0;
+
+}
