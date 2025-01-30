feefc14cff0d35569a6f1d365dd6520fa4ac8f74
Vishal Yadav <67411296+vishalyl02@users.noreply.github.com>
2025-01-30 22:04:58 +0530

    Update 1_Sum_of_Numbers.cpp

--- Lecture-04  ( Problems on Loops )/1_Sum_of_Numbers.cpp
+++ Lecture-04  ( Problems on Loops )/1_Sum_of_Numbers.cpp
@@ -15,7 +15,7 @@ int main()
     int sum = 0;
     for (int i = 0; i < n; i++)
     {
-        sum = sum + arr[i];
+        sum = sum + arr[i];    // Adding the values of each elements of array 
     }
 
     cout << sum << endl;
